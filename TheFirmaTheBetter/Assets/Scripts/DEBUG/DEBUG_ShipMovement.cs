81df39029fa3b4738f2ee9c2ba178f4df48ff9d7
Steef Weijdeven <steefiscool70@gmail.com>
2022-09-13 11:05:58 +0200

    moved ship parts scripts to own folder

--- TheFirmaTheBetter/Assets/Scripts/DEBUG/DEBUG_ShipMovement.cs
+++ TheFirmaTheBetter/Assets/Scripts/DEBUG/DEBUG_ShipMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class DEBUG_ShipMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float EngineSpeed = 1f;
+    [SerializeField]
+    private float HullIntegrity = 1f;
+
+    public void DEBUG_Engine()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DEBUG_Hull()
     {
-        
+
+    }
+
+    public void DEBUG_Weapon()
+    {
+
+    }
+
+    public void DEBUG_Special()
+    {
+
     }
 }
