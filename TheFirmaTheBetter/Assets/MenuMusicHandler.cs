c30ee80d08c46094c9f504a78c755070a2782b42
Julian <Julian-Tekstra@hotmail.com>
2022-10-11 14:50:15 +0200

    Stop building theme on battle start

--- TheFirmaTheBetter/Assets/MenuMusicHandler.cs
+++ TheFirmaTheBetter/Assets/MenuMusicHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EventSystem;
 
 public class MenuMusicHandler : MonoBehaviour
 {
@@ -12,7 +13,8 @@ public class MenuMusicHandler : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         fmodEvent = GetComponent<FMODUnity.StudioEventEmitter>();
-        buildingTheme = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Mus_BuildTheme"); 
+        buildingTheme = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Mus_BuildTheme");
+        Channels.OnEveryPlayerReady += LoadBattleScene;
     }
 
     // Update is called once per frame
@@ -24,5 +26,11 @@ public class MenuMusicHandler : MonoBehaviour
     {
         fmodEvent.Stop();
         buildingTheme.start();
+    }
+
+    public void LoadBattleScene(int playerCount)
+    {
+        //todo: call battle music
+        buildingTheme.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 }
