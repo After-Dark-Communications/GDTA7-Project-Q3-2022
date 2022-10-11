40f589cae80f275f85f419e5b1a0e7b769b1213a
Roberta Netzova <robertanetzova@ranj.nl>
2022-10-11 18:08:47 +0200

    DEV: Pickups appear and get destoroyed at their own time.

--- TheFirmaTheBetter/Assets/Project/Scripts/Pickups/PickupManager.cs
+++ TheFirmaTheBetter/Assets/Project/Scripts/Pickups/PickupManager.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pickups;
 
 public class PickupManager : MonoBehaviour
 {
+    [SerializeField]
+    private float pickupLiveTime;
+
+    private TimeTracker timeTrack;
+
+
+    private void Awake()
+    {
+        timeTrack = new TimeTracker(pickupLiveTime);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,12 @@ public class PickupManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!timeTrack.TimerComplete())
+        {
+            return;
+        }
+        Debug.Log("Destroyed");
+        Destroy(this.gameObject);
     }
+
 }
--- TheFirmaTheBetter/Assets/Project/Scripts/Pickups/PickupZoneSpawner.cs
+++ TheFirmaTheBetter/Assets/Project/Scripts/Pickups/PickupZoneSpawner.cs
@@ -1,71 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pickups;
 
-public class PickupZoneSpawner : MonoBehaviour
-{
-    private float instantiateTime;
-    public Vector3 center;
-    [SerializeField]
-    private GameObject[] pickups;
-    [SerializeField]
-    private Vector3 spawnAreaSize;
 
-    [SerializeField]
-    private float spawnInterval;
-
-    private void Awake()
+    public class PickupZoneSpawner : MonoBehaviour
     {
-        center = this.gameObject.transform.position;
-        instantiateTime = spawnInterval;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+        //private float instantiateTime;
+        public Vector3 center;
+        [SerializeField]
+        private GameObject[] pickups;
+        [SerializeField]
+        private Vector3 spawnAreaSize;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //For debug
-        //if (Input.GetKeyDown(KeyCode.Space))
+        TimeTracker timeTrack;
+
+        [SerializeField]
+        private float spawnInterval;
+
+        private void Awake()
+        {
+            center = this.gameObject.transform.position;
+
+            timeTrack = new TimeTracker(spawnInterval);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //For debug
+            //if (Input.GetKeyDown(KeyCode.Space))
+            //{
+
+            //}
+
+            //if (!isSpawnTime())
+            //{
+            //    return;
+            //}
+
+            if (!timeTrack.TimerComplete())
+            {
+                return;
+            }
+
+            Spawn();
+            timeTrack.TimeReset();
+
+        }
+        //private bool isSpawnTime()
         //{
-            
+        //    instantiateTime -= Time.deltaTime;
+        //    if (instantiateTime <= 0)
+        //    {
+        //        return true;
+        //    }
+        //    return false;
         //}
-
-        if(!isSpawnTime())
+        //private void TimeReset()
+        //{
+        //    instantiateTime = spawnInterval;
+        //}
+        private void Spawn()
         {
-            return;
+            int randomIndex = Random.Range(0, pickups.Length);
+            Vector3 randomSpawnPosition = center + new Vector3(Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2), Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2), Random.Range(-spawnAreaSize.z / 2, spawnAreaSize.z / 2));
+
+            Instantiate(pickups[randomIndex], randomSpawnPosition, Quaternion.identity);
         }
-
-        Spawn();
-        TimeReset();
-
-    }
-    private bool isSpawnTime()
-    {
-        instantiateTime -= Time.deltaTime;
-        if(instantiateTime <=0)
+        private void OnDrawGizmosSelected()
         {
-            return true;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, spawnAreaSize);
         }
-        return false;
+    
     }
-    private void TimeReset()
-    {
-        instantiateTime = spawnInterval;
-    }
-    private void Spawn()
-    {
-        int randomIndex = Random.Range(0, pickups.Length);
-        Vector3 randomSpawnPosition = center + new Vector3(Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2), 0, Random.Range(-spawnAreaSize.z / 2, spawnAreaSize.z / 2));
-
-        Instantiate(pickups[randomIndex], randomSpawnPosition, Quaternion.identity);
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(transform.position, spawnAreaSize);
-    }
-}
