38c663fc8f07d0cd96148075841dac1bd1718b5d
Roberta Netzova <robertanetzova@ranj.nl>
2022-10-12 11:22:03 +0200

    VD: models  for pickups added

--- TheFirmaTheBetter/Assets/Project/Scripts/Pickups/PickupManager.cs
+++ TheFirmaTheBetter/Assets/Project/Scripts/Pickups/PickupManager.cs
@@ -2,14 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pickups;
+using Collisions;
+using ShipParts.Ship;
 
-public class PickupManager : MonoBehaviour
+public class PickupManager : MonoBehaviour, ICollidable
 {
     [SerializeField]
     private float pickupLiveTime;
 
     private TimeTracker timeTrack;
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ICollidable collisionObject = other.GetComponentInParent<ICollidable>();
+
+        if (collisionObject != null)
+        {
+            if (collisionObject is Ship)
+                return;
+
+            //impactSpawner.SpawnImpactHitPrefab();
+            collisionObject.HandleCollision(this, null);
+            Debug.Log("Collision");
+        }
+    }
+    public void DestroySelf()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void HandleCollision<T1>(T1 objectThatHit, ShipStats shipStats) where T1 : ICollidable
+    {
+        throw new System.NotImplementedException();
+    }
 
     private void Awake()
     {
@@ -28,7 +54,6 @@ public class PickupManager : MonoBehaviour
         {
             return;
         }
-        Debug.Log("Destroyed");
         Destroy(this.gameObject);
     }
 
