53a98eb294bce86b7a6d60c1b1a82b0fa3a5d0da
RickvanHam <428294@student.fontys.nl>
2022-10-14 18:15:50 +0200

    Start on Map Selection (Prototype for Open day Testing)

---
+++ TheFirmaTheBetter/Assets/Project/Scripts/MapSelection/FFAMapData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFFAMapData", menuName = "Map/Create new FFAMapData")]
+public class FFAMapData : MapData
+{
+    
+}
