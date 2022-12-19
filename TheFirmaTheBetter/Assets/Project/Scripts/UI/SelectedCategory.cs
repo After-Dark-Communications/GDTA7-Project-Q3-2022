330f3cc60179ca4b252a831fa35191ed0d95ce4b
Roberta Netzova <roberta.netzova@gmail.com>
2022-12-19 15:18:21 +0100

    VD: Ship Selection adding all categories in a list. Still animator is not working properly.

--- TheFirmaTheBetter/Assets/Project/Scripts/UI/SelectedCategory.cs
+++ TheFirmaTheBetter/Assets/Project/Scripts/UI/SelectedCategory.cs
@@ -12,12 +12,39 @@ public class SelectedCategory : MonoBehaviour
 
     private PlayerSelectionScreen playerSelectionScreen;
 
+    private const string TRIGGER_ENABLE_NAME = "Enable";
+    private const string TRIGGER_DISABLE_NAME = "Disable";
+    private Animator animator;
+
+    private void OnEnable()
+    {
+        Channels.OnNavigateUp += CategoryChangedListUP;
+        Channels.OnNavigateDown += CategoryChangedListDOWN;
+    }
+
+    private void CategoryChangedListDOWN(int playerNumber)
+    {
+       
+    }
+
+    private void CategoryChangedListUP(int playerNumber)
+    {
+        throw new NotImplementedException();
+    }
+
     private void Start()
     {
         tmpTextField = GetComponent<TMP_Text>();
         playerSelectionScreen = GetComponentInParent<PlayerSelectionScreen>();
 
-        Channels.OnSelectedCategoryChanged += CategoryChanged;
+        Channels.OnSelectedCategoryChanged += CategoryChangedList;
+
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnDestroy()
+    {
+        Channels.OnSelectedCategoryChanged -= CategoryChangedList;
     }
 
     private void CategoryChanged(SelectableCollection currentSelectedCollection, int playerNumber)
@@ -26,5 +53,30 @@ public class SelectedCategory : MonoBehaviour
             return;
 
         tmpTextField.SetText(currentSelectedCollection.CategoryName);
+    }
+
+    private void CategoryChangedList (SelectableCollection currentSelectedCollection, int playerNumber)
+    {
+        if (playerSelectionScreen.PlayerNumber != playerNumber)
+            return;
+
+        String current = currentSelectedCollection.CategoryName.ToLower();
+        String field = tmpTextField.text.ToLower();
+        if (String.Compare(current, field) == 0)
+        {
+            Debug.Log($"equal: " + current + ' ' + field);
+            animator.SetTrigger(TRIGGER_ENABLE_NAME);
+            return;
+        }
+        else
+        {
+            Debug.Log($"--not " + current + ' ' + field);
+            animator.SetTrigger(TRIGGER_DISABLE_NAME);
+        }
+    }
+
+    public void PlaySelectedAnimation()
+    {
+       //animator.SetTrigger(TRIGGER_NAME);
     }
 }
