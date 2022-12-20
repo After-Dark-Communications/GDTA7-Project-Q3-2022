46eccb0f3286bf3e4a60d9384a76845fd48bd4a6
Martin Dzhurov <m.dzurov@student.fontys.nl>
2022-12-20 13:01:25 +0200

    Delete #

--- TheFirmaTheBetter/Assets/Project/Scripts/UI/SelectedCategory.cs
+++ TheFirmaTheBetter/Assets/Project/Scripts/UI/SelectedCategory.cs
@@ -29,7 +29,7 @@ public class SelectedCategory : MonoBehaviour
     {
         Channels.OnSelectedCategoryChanged -= CategoryChangedList;
     }
-#
+
     /// <summary>
     /// Old solution
     /// </summary>
